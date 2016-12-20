626e0d87655d8fa9621737a6cf39eb5a7d2e224e
Chef User <chef@localhost.localdomain>
2016-12-20 18:07:53 +0000

    Change content and version

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -16,3 +16,7 @@ describe port(80) do
   it { should_not be_listening }
   skip 'This is an example test, replace with your own test.'
 end
+
+describe command('curl localhost') do
+	its('stdout') {should match('Hello, world')}
+end
